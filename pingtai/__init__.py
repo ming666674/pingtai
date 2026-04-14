a0aaa6a76066ffaaa7e7a889e790c50d3289eae8
lwm <2933663621@qq.com>
2026-04-14 10:45:31 +0800

    feat：增加一个拦截器来进行登录判断

--- pingtai/__init__.py
+++ pingtai/__init__.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask,request,session,redirect
 
+def auth():
+    if request.path.startswith("/static"):
+        # 继续向后执行，不拦截
+        return 
+    
+    if request.path == '/login':
+        #继续向后执行，不拦截
+        return
+    
+    user_info = session.get("user_info")
+    if user_info:
+        #继续向后执行，不拦截
+        return 
+
+    return redirect("/login")
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +28,5 @@ def create_app():
     app.register_blueprint(account.ac)
     app.register_blueprint(order.od)
     
-
+    app.before_request(auth)
     return app
