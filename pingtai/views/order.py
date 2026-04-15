077afd5d82c99d7d2b5fcf1f4175f4c93ec85221
lwm <2933663621@qq.com>
2026-04-15 15:26:38 +0800

    refactor：连表查询

--- pingtai/views/order.py
+++ pingtai/views/order.py
@@ -11,11 +11,11 @@ def order_list():
     role = user_info['role'] #1-客户  2-管理员
     if role == 2:
        # select * from order 
-       data_list = db.fetch_all("select * from `order`", [])
-       
+       # data_list = db.fetch_all("select * from `order`", [])
+       data_list = db.fetch_all("select * from `order` left join userinfo on `order`.user_id = userinfo.id;", [])
     else:
         # select * from order where user_id = user_info['id']
-        data_list = db.fetch_all("select * from `order` where user_id =%s",[user_info['id'],])
+        data_list = db.fetch_all("select * from `order` left join userinfo on `order`.user_id = userinfo.id where `order`.user_id =%s",[user_info['id'],])
         
     print(data_list)
     return render_template("order_list.html",data_list=data_list)
